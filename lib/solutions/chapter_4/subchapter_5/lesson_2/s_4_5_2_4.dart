e50e7decf05caa7466431050e2bb7151bcdd35a7
Lukas Schmidt <129980440+LumiToad@users.noreply.github.com>
2025-09-18 11:23:56 +0200

    4.4.1_TS_Stateful_Widgets

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,45 @@ class S4524 extends StatelessWidget {
   const S4524({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ShowHideNameWidget();
+  }
+}
+
+
+class ShowHideNameWidget extends StatefulWidget {
+  const ShowHideNameWidget({super.key});
+
+  @override
+  State<ShowHideNameWidget> createState() => _ShowHideNameWidgetState();
+}
+
+class _ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  _ShowHideNameWidgetState(){
+    currentButtonText = buttonTextShow;
+  }
+
+  String myName = "LumiToad";
+  String currentNameText = "";
+
+  String buttonTextShow = "Name anzeigen";
+  String buttonTextHide = "Name verstecken";
+  String currentButtonText = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(currentNameText),
+        ElevatedButton(
+          onPressed: () { 
+            setState(() {
+                currentNameText = (currentNameText == "") ? myName : "";
+                currentButtonText = (currentButtonText == buttonTextShow) ? buttonTextHide : buttonTextShow;
+              });
+            },
+          child: Text(currentButtonText)
+          )
+      ],
+    );
   }
 }
