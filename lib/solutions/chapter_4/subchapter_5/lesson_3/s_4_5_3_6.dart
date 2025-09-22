28757cc703cf889ed841e2564d7b09dcaae61b63
Lukas Schmidt <129980440+LumiToad@users.noreply.github.com>
2025-09-22 12:47:49 +0200

    4.4.2_TS_Navigation_Bar_Custom_Widgets

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -1,9 +1,52 @@
 import 'package:flutter/material.dart';
 
+class Product {
+  const Product({required this.name, required this.price});
+
+  final String name;
+  final double price;
+}
+
 class S4536 extends StatelessWidget {
   const S4536({super.key});
+
+  final List<Product> products = const [
+    Product(name: "Kekse", price: 1.69),
+    Product(name: "Butter", price: 2.99),
+    Product(name: "Cola", price: 1.25),
+    Product(name: "Kaffee", price: 20.00),
+    Product(name: "Shampoo", price: 1.30),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ListView(
+      shrinkWrap: true,
+      children: List.generate(products.length, (index) {
+        return ListTile(
+          leading: Icon(Icons.shopping_cart),
+          title: FormatedProductLabel(product: products[index]),
+          );
+        }),
+    );
+  }
+}
+
+class FormatedProductLabel extends StatelessWidget {
+  const FormatedProductLabel({
+    super.key,
+    required this.product,
+  });
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "${product.name} " + 
+       "${product.price}".padRight(4, "0") +
+       ((product.price % 2 == 0) ? "0" : "" )+
+       " €"
+      );
   }
 }
